f756579a9dd8bc9f8deb0a1064faf5bae2467293
Oh seungmin <tmdals179@gmail.com>
2024-02-19 09:21:43 +0900

    refactor: separate js files by purpose main.js session.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,23 +117,3 @@ function setTheme(themeToSet, targets) {
         target.querySelector("#dark-theme-toggle-screen-reader-target").textContent = [THEME_TO_ICON_TEXT_CLASS[themeToSet]];
     });
 }
-
-
-// 세션 ID를 가져오는 함수
-function getSessionId() {
-    // 세션 ID가 이미 존재하는지 확인
-    let sessionId = localStorage.getItem('sessionId');
-
-    // 세션 ID가 없으면 새로 생성
-    if (!sessionId) {
-        sessionId = generateSessionId();
-        localStorage.setItem('sessionId', sessionId);
-    }
-
-    return sessionId;
-}
-
-// 임의의 세션 ID를 생성하는 함수
-function generateSessionId() {
-    return 'session_' + Math.random().toString(36).substr(2, 9); // 임의의 문자열 생성
-}
